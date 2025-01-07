a23a3f248c9ff62d8e5cd1be5469776167e3f885
jamesattoh <koss.attoh27@gmail.com>
2025-01-07 23:29:46 +0100

    first routing file & Link using

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Header from './components/Header'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/survey' element={ <Survey />} />
+        <Route />
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
