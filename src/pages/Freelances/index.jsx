1095636c1589d0b98addba8d2df696e7fc739cd3
jamesattoh <koss.attoh27@gmail.com>
2025-01-26 10:43:34 +0100

    typing props with PropTypes

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,37 @@
-function Freelances() {
+import DefaultPicture from '../../assets/profile.png'
+import Card from '../../components/Card'
+
+    function Freelances() {
+
+      const freelanceProfiles = [
+        {
+            name: 'Jane Doe',
+            jobTitle: 'Devops',
+            picture: DefaultPicture,
+        },
+        {
+            name: 'John Doe',
+            jobTitle: 'Developpeur frontend',
+            picture: DefaultPicture,
+        },
+        {
+            name: 'Jeanne Biche',
+            jobTitle: 'Développeuse Fullstack',
+            picture: DefaultPicture,
+        },
+    ]
+
     return (
       <div>
-        <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+        <h1>Freelances 👩·💻👨·💻👩·💻</h1>
+        {freelanceProfiles.map((profile, index) => (
+            <Card
+              key={`${profile.name}-${index}`}
+              label={profile.jobTitle}
+              picture={profile.picture}
+              title={profile.name}
+            />
+        ))}
       </div>
     )
   }
