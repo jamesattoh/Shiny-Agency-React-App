c0c8bde65ace136c67fd5a0f157296ebc234d407
jamesattoh <koss.attoh27@gmail.com>
2025-01-30 01:12:10 +0100

    header & homepage done

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,35 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import colors from '../../utils/style/colors' //defini dans src/utils/colors
 
-const StyledLink = styled(Link)`
-    padding: 15px;
-    color: #8186a0;
-    text-decoration: none;
-    font-size: 18px;
-    ${(props) =>
-        props.$isFullLink &&
-        `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+import { StyledLink } from '../../utils/style/Atoms'
+import DarkLogo from '../../assets/dark-logo.png'
+
+const HomeLogo = styled.img`
+    height: 78px
+`
+
+const NavContainer = styled.nav`
+    padding: 30px;
+    display: flex;
+    justify-content: space-between; //l'axe horizontal
+    align-items: center; //l'axe vertical
 `
 
 function Header() {
     return (
-        <nav>
-            <StyledLink to="/">Accueil</StyledLink>
-            <StyledLink to="/survey/1">Questionnaire</StyledLink>
-            <StyledLink to="/freelances">Profils</StyledLink>
-            <StyledLink to="/survey/1" $isFullLink> {/*$ => signaler a styled-components  que notre prop nous sert pour le style, et qu'elle ne doit pas être passée dans le DOM; $ pour composant react, sans le $ pour simple balise comme a */}
-                Faire le test
-            </StyledLink>
-        </nav>
+        <NavContainer>
+            <Link to="/">
+                <HomeLogo src={DarkLogo}/>
+            </Link>
+            <div>
+                <StyledLink to="/">Accueil</StyledLink>
+                <StyledLink to="/survey/1">Questionnaire</StyledLink>
+                <StyledLink to="/freelances">Profils</StyledLink>
+                <StyledLink to="/survey/1" $isFullLink> {/*$ => signaler a styled-components  que notre prop nous sert pour le style, et qu'elle ne doit pas être passée dans le DOM; $ pour composant react, sans le $ pour simple balise comme a */}
+                    Faire le test
+                </StyledLink>
+            </div>
+        </NavContainer>
     )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,54 @@
+import styled from "styled-components"
+import HomeIllustration from "../../assets/home-illustration.svg"
+import colors from "../../utils/style/colors"
+import { StyledLink } from "../../utils/style/Atoms"
+
+const Illustration = styled.img `
+  flex: 1;
+`
+const HomeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+const HomeContainer = styled.div`
+  margin: 30px;
+  padding: 60px 90px;
+  display: flex;
+  flex-direction: row;
+  max-width: 1200px;
+  background-color: ${colors.backgroundLight};
+`
+const LeftCol = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1; //occuper l'espace disponible 
+  ${StyledLink}{
+    max-width: 200px;
+  }
+`
+const StyledTitle = styled.h2`
+  padding-bottom: 30px;
+  max-width: 250px;
+  line-height: 50px;
+`
+
 function Home() {
   return (
-    <div>
-      <div>Page d'accueil 🏡</div>
-    </div>
+    <HomeWrapper>
+      <HomeContainer>
+        <LeftCol>
+          <StyledTitle>
+            Repérez vos besoins, on s'occupe du reste, avec les meilleurs talents
+          </StyledTitle>
+          <StyledLink to="/survey/1" $isFullLink>
+            Faire le test
+          </StyledLink>
+        </LeftCol>
+        <Illustration src={HomeIllustration}/>
+      </HomeContainer>
+    </HomeWrapper>
+    
   )
 }
 
