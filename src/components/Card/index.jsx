005f8391e6c2ff6b3c0ee5171fbb006e39af89b2
jamesattoh <koss.attoh27@gmail.com>
2025-01-30 11:43:31 +0100

    error and profiles pages done

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,14 +6,22 @@ import colors from "../../utils/style/colors"
 const CardLabel = styled.span`
     color: #5843e4;
     font-size: 22px;
-    font-weight: bold;
+    font-weight: normal;
+    padding-left: 15px;
 `
 
 const CardImage = styled.img`
-    height: 80px;
-    width: 80px;
+    height: 150px;
+    width: 150px;
+    align-self: center;
     border-radius: 50%;
-    border: 2px solid #5843e4;
+`
+
+const CardTitle = styled.span`
+    color: black;
+    font-size: 22px;
+    font-weight: normal;
+    align-self: center;
 `
 
 const CardWrapper = styled.div`
@@ -21,21 +29,23 @@ const CardWrapper = styled.div`
     flex-direction: column;
     padding: 15px;
     background-color: ${colors.backgroundLight};
+    justify-content: space-around;
     border-radius: 30px;
     width: 350px;
     transition: 200ms;
     &:hover {
-        cursor: pointer;
+        cursor: pointer; //change le curseur fleche en curseur "main"
         box-shadow: 2px 2px 10px #e2e3e9;
     }
+
 `
 
 function Card({ label, title, picture,}) {
     return (
-        <CardWrapper style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
+        <CardWrapper>
             <CardLabel>{label}</CardLabel>
             <CardImage src={picture} alt="freelance"/>
-            <span>{title}</span>
+            <CardTitle>{title}</CardTitle>
         </CardWrapper>
     )
 }
--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,6 +1,8 @@
 import Card from '../../components/Card'
 import DefaultPicture from '../../assets/profile.png'
 import styled from 'styled-components'
+
+import colors from '../../utils/style/colors'
  
 const freelanceProfiles = [
     {
@@ -18,21 +20,43 @@ const freelanceProfiles = [
         jobTitle: 'Développeuse Fullstack',
         picture: DefaultPicture,
     },
+    {
+      name: 'Lorem ipsum',
+      jobTitle: 'UX Designer',
+      picture: DefaultPicture,
+    },
 ]
 
 
 const CardsContainer = styled.div`
-    display: grid;
-    gap: 24px;
-    grid-template-rows: 350px 350px;
-    grid-template-columns: repeat(2, 1fr);
+  display: grid;
+  gap: 50px;
+  grid-template-rows: 350px 350px;
+  grid-template-columns: repeat(2, 1fr);
+  justify-items: center;
 `
+const PageTitle = styled.h1`
+  font-size: 30px;
+  color: black;
+  font-weight: 300;
+  text-align: center;
+  padding-bottom: 30px;
+`
+const PageSubtitle = styled.h2`
+  font-size: 20px;
+  color: ${colors.secondary};
+  font-weight: 300;
+  text-align: center;
+  padding-bottom: 30px;
+`
+
 
 function Freelances() {
 
   return (
     <div>
-      <h1>Freelances 👩·💻👨·💻👩·💻</h1>
+      <PageTitle>Trouvez votre prestataire</PageTitle>
+      <PageSubtitle>Chez Shiny, nous réunissons les meilleurs profils pour vous.</PageSubtitle>
       <CardsContainer>
         {freelanceProfiles.map((profile, index) => (
             <Card
